67e8873f21b2d65fd71d6446a40643c3ec604946
black2car <black2car+prayogi.pangestu1299@gmail.com.noreply.github.com>
2024-12-20 18:53:08 +0700

    Add contactPage function to contact module

--- src/contact.js
+++ src/contact.js
@@ -1,7 +1,57 @@
 import { Card } from "./home.js";
+import phoneIcon from "./images/contact-phone.png";
+import emailIcon from "./images/contact-email.png";
+import fbIcon from "./images/fb.png";
+import igIcon from "./images/ig.png";
+import xIcon from "./images/x.png";
 
-class ContactCard extends Card{
-  constructor(title, description, image, page){
-    super((title, description, image, page))
+class ContactCard extends Card {
+  constructor( title, description, image, page){
+    super( title, description, image, page );
   }
 }
+
+const page = 'contact';
+const contact = [
+  new ContactCard(
+    'Phone',
+    '+62 888 888 888',
+    phoneIcon,
+    page
+  ),
+  new ContactCard(
+    'Email',
+    'wkitchen@email.com',
+    emailIcon,
+    page
+  ),
+  new ContactCard(
+    'Facebook',
+    'w_kitchen',
+    fbIcon,
+    page
+  ),
+  new ContactCard(
+    'Instagram',
+    '@w_kitchen',
+    igIcon,
+    page
+  ),
+  new ContactCard(
+    'X',
+    '@w_kitchen',
+    xIcon,
+    page
+  ),
+];
+
+const contactPage = function(){
+  const container = document.createElement('div');
+  container.classList.add(`container-${page}`)
+  contact.forEach(function(card){
+    container.appendChild(card.generateCardNode('35px'));
+  });
+  return container;
+}
+
+export { contactPage };
