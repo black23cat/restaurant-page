bd1513bfa420782789c7f367f1fb7d08522a7193
black2car <black2car+prayogi.pangestu1299@gmail.com.noreply.github.com>
2024-12-20 07:49:04 +0700

    Add event listener to home, menu and contact button

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
 import "./styles.css";
 
-console.log('hello');
+const screenDisplay = function (){
+  const buttons = document.querySelectorAll('nav button');
+  buttons.forEach(function(button){
+    button.addEventListener('click', btnClickHandler);
+  });
+
+  function btnClickHandler(e){
+    const content = document.getElementById('content');
+    switch(e.target.classList.value){
+      case 'home':
+        alert(`You click ${e.target.classList.value}`);
+        break;
+      case 'menu':
+        alert(`You click ${e.target.classList.value}`);
+        break;
+      case 'contact':
+        alert(`You click ${e.target.classList.value}`);
+        break;
+    }
+  }
+
+  // Initial page load
+}();
